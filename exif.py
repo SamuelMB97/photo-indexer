840bdba3a538e4157e2d16374b4bcd0226304321
SamuelMB97 <43507135+SamuelMB97@users.noreply.github.com>
2026-09-01 23:19:40 -0600

    Added exif file and mostly got it working

---
+++ exif.py
@@ -0,0 +1,80 @@
+"""Disclaimer: most of this file is copied from my group's work on our
+in-class assignment to work on this problem. I've adapted it to work
+for my code, but I do understand how it's all working""" 
+
+import exifread
+#import sys
+import reverse_geocoder as rg
+  
+
+
+def get_timestamp(file):
+    with open(file, "rb") as image:
+        tags = exifread.process_file(image)
+            
+
+    date = tags.get("EXIF DateTimeOriginal")
+    if date:
+        return date
+    else:
+        return None
+
+def get_coordiantes(file):
+    with open(file, "rb") as image:
+        tags = exifread.process_file(image)
+
+    if not tags:
+        return None, None
+
+    latitude = tags.get("GPS GPSLatitude")
+    longitude = tags.get("GPS GPSLongitude")
+
+    latref = tags.get("GPS GPSLatitudeRef")
+    longref = tags.get("GPS GPSLongitudeRef")
+
+    print(f"got coordinates: {latitude} {latref}, {longitude} {longref}")
+    return convert_to_decimal(latitude, latref), convert_to_decimal(longitude, longref)
+
+def convert_to_decimal(dms, reference):
+    degrees = dms.values[0]
+    minutes = dms.values[1]
+    seconds = dms.values[2]
+
+    decimal = int(degrees) + int(minutes)/60 + float(seconds)/3600
+    if str(reference) == "S" or str(reference) == "W":
+        decimal*= -1
+
+    print("converted to decimal")
+    return decimal
+
+
+def convert_to_location(lat, long):
+    print("converting to location...")
+    coordinates = (lat, long)
+    loc_data= rg.search(coordinates)[0]
+    #loc_data = loc_data_big[0]
+    print(f"{loc_data["name"]}, {loc_data["admin1"]}, {loc_data["cc"]}")
+    city = loc_data["name"]
+    state = loc_data["admin1"]
+    country = loc_data["cc"]
+    return (city, state, country)
+
+
+def exif_to_location(file):
+    lat, long = get_coordiantes(file)
+    if lat and long:
+        print(f"lat: {lat}, long: {long} --> Let's convert to location")
+        return convert_to_location(lat, long)
+    else:
+        return None
+
+"""def main():
+    image = sys.argv[1]
+    lat, long = get_coordiantes(image)
+
+    convert_to_location(lat,long)
+    
+
+
+if __name__ == "__main__":
+     main()"""
--- indexer.py
+++ indexer.py
@@ -1,5 +1,9 @@
 import sys
 import os
+import exif
+
+
+
 
 
 def is_image(file_name):
@@ -33,7 +37,7 @@ def index_image_dir_recursive(directory, recursive_depth=1):
 
             if sub_dir_data["images"] or sub_dir_data["directories_with_images"]:
                 dir_data["directories_with_images"].append(entry)
-                create_index(sub_dir_data) # TODO make this function and one that it references to set up images
+                create_index(entry, sub_dir_data) # TODO make this function and one that it references to set up images
 
         # TODO make this elif vv reference a helper function
         # CASE 2 IMAGE
@@ -51,10 +55,88 @@ def index_image_dir_recursive(directory, recursive_depth=1):
     return dir_data
         
 
-def create_index(entry):
-    pass
-    #with open("this should be the index name?") as d:
+def create_index(parent_dir, sub_dir_data):
+    index_path = os.path.join(parent_dir.path, "index.html")
+    with open(index_path, "w", encoding="utf-8") as html:
+        html.write(create_html_str(parent_dir, sub_dir_data))
 
+
+def create_html_str(parent_dir, sub_dir_data):
+    sub_dir_links = create_sub_dir_str(sub_dir_data)
+    images_html = create_images_html(sub_dir_data)
+
+    style = """
+        <style>
+        .dirlink_el {font-size: 125%;}
+        .sm_img {max-height: 200px; max-width: 200px;}
+        .img_el {display: inline-block; padding-bottom: 15px; padding-right: 10px;}
+        </style>""" #style is largely copied from example html
+
+    html_str = f"""
+<html>
+    <head>{style}
+    </head>
+    <body>
+        <h2> {parent_dir.name} </h2>
+        {sub_dir_links}
+        <br>
+        {images_html}
+    </body>
+</html>"""
+    return html_str
+
+
+def create_sub_dir_str(sub_dir_data):
+    sub_dir_str = ""
+
+    for sub_dir in sub_dir_data["directories_with_images"]:
+        sub_dir_str = sub_dir_str + f"""
+        <div class="dirlink_el">
+            <a href={sub_dir.name + r"\index.html"}>{sub_dir.name}</a>
+        </div>"""
+    
+    return sub_dir_str
+
+
+def create_images_html(sub_dir_data): # currently attaching the images too large
+    images_str = ""
+
+    for image in sub_dir_data["images"]:
+        images_str = images_str + f"""
+        <div class="img_el">
+            <a href={image.name}>
+                <img src={image.name} class="sm_img">
+            </a>
+            <br>
+            {image.name}
+            {get_exif_data(image)}
+        </div>"""
+
+    return images_str
+
+
+def get_exif_data(image): # TODO This works great for the timestamp, but isn't working at all for location
+    print(f"Let's get exif data for image: {image.name}")
+    date_time = exif.get_timestamp(image)
+    loc = exif_to_location(image)
+    return f"""
+            <br>
+            {date_time if date_time else ""}
+            <br>
+            {loc if loc else ""}"""
+
+
+def exif_to_location(image):
+    lat, long = exif.get_coordiantes(image)
+    print(f"lat: {lat}, long: {long}")
+    try:
+        loc = exif.convert_to_location(lat, long)
+        print(f"\tLOCATION: {loc}")
+        city_state_country = f"{loc[0]}, {loc[1]} {loc[2]}"
+        return city_state_country
+    except:
+        print("\tCouldn't get the location data...")
+        return None
 
 
 def main():
