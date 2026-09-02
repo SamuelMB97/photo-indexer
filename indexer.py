1dadb6a5e8006c9be3f785e618e2305db0d2d826
SamuelMB97 <43507135+SamuelMB97@users.noreply.github.com>
2026-09-01 19:43:56 -0600

    refined recursive call to return internal data
    data needed to create current level index

--- indexer.py
+++ indexer.py
@@ -2,6 +2,13 @@ import sys
 import os
 
 
+def is_image(file_name):
+    if file_name.lower().endswith((".jpg", ".jpeg", ".png", ".gif")):
+        return True
+    else:
+        return False
+
+
 def index_image_dir_recursive(directory, recursive_depth=1):
     """Searches directory recursively, indexing folders with images
     inside
@@ -13,40 +20,41 @@ def index_image_dir_recursive(directory, recursive_depth=1):
 
     depth_tab = "|\t"*recursive_depth
     print(f"{depth_tab}scanning {directory}")
-    images_inside = False
-    images = []
-    directories = []
+    dir_data = {"directories_with_images": [], "images": []}
 
     for entry in os.scandir(directory):
 
-        if entry.is_dir(): # Case 1 directory
+        # CASE 1 DIRECTORY
+        if entry.is_dir(): 
             print(f"{depth_tab}Found DIRECTORY")
-            sub_has_images = index_image_dir_recursive(entry, recursive_depth+1) # check for images inside
 
-            if sub_has_images:
-                images_inside = True
-                create_index() # TODO make this function and one that it references to set up images
+            #Recursive call vv
+            sub_dir_data = index_image_dir_recursive(entry, recursive_depth+1) # check for images inside
+
+            if sub_dir_data["images"] or sub_dir_data["directories_with_images"]:
+                dir_data["directories_with_images"].append(entry)
+                create_index(sub_dir_data) # TODO make this function and one that it references to set up images
 
         # TODO make this elif vv reference a helper function
-        elif entry.name.lower().endswith((".jpg", ".jpeg", ".png", ".gif")): # Case 2 image 
+        # CASE 2 IMAGE
+        elif is_image(entry.name):
             print(f"{depth_tab}Found IMAGE")
-            # set up image
-            images.append(entry)
-            images_inside = True
+            dir_data["images"].append(entry)
 
+        # CASE 3 NOT A DIRECTORY OR AN IMAGE
         else:
             print(f"{depth_tab}Found NON IMAGE/DIR")
 
     
-    print(f"{depth_tab}done scanning {directory}, has images inside? {images_inside}")
+    print(f"{depth_tab}done scanning {directory}, has images inside? {bool(dir_data["images"])}")
 
-    if images_inside:
-        return True
-    else: return False
+    return dir_data
         
 
-def create_index():
+def create_index(entry):
     pass
+    #with open("this should be the index name?") as d:
+
 
 
 def main():
